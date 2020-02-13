6df0c45871c6fe3d1b27c7c3b54ab8491f2d1db0
= Nana Adjei <nana.elvee@gmail.com>
2020-02-13 17:12:19 +0000

    Variables names re-constructed ✅

--- src/Mobiforte.php
+++ src/Mobiforte.php
@@ -42,9 +42,9 @@ class Mobiforte
      * $sender_id is the name that will oppear on the receiver's phone.
      * This can be overwritten.
      */
-    public function from($sender_id)
+    public function from($senderId)
     {
-        return $this->sender_id = $sender_id;
+        $this->sender_id = $senderId;
 
         return $this;
     }
@@ -59,26 +59,40 @@ class Mobiforte
     }
 
     /**
-     * Send Sms to Phone number
-     * @param  int $to recepient phone number
-     * @param  string $message    messge to be sent
-     * @param  string $date_time 	schedule
+     * You can send sms with different api client id and secrete
+     * @param $clientId is the fresh client id;
+     * @param $clientSecret is the fresh client secret;
      */
-    public function send($to = null, $message = null, $date_time = null)
+    public function withFreshApiKeys(string $clientId, string $clientSecret)
+    {
+        $this->client_id = $clientId;
+
+        $this->client_secret = $clientSecret;
+
+        return $this;
+    }
+
+    /**
+     * Send Sms to Phone number
+     * @param  int $to recepient phone number.
+     * @param  string $message    messge to be sent.
+     * @param  string $dateTime 	schedule.
+     */
+    public function send($to = null, $message = null, $dateTime = null)
     {
         $this->to = $to ?: $this->to;
         $this->message = $message ?: $this->message;
-        $this->date_time = $date_time ?: $this->date_time;
+        $this->date_time = $dateTime ?: $this->dateTime;
 
         /** Construct URL */
-        $url = static::SMS_ENDPOINT . "?"
-            . "client_id=" . $this->client_id .
+        $url = static::SMS_ENDPOINT . "?" .
+            "client_id=" . $this->client_id .
             "&client_secret=" . $this->client_secret .
             "&recipient=" . $this->to .
             "&sender_id=" .  substr($this->sender_id, 0, 11) .
             "&message=" . urlencode($this->message);
 
-        if (!is_null($date_time)) $url .= "&date_time" . $date_time;
+        if (!is_null($dateTime)) $url .= "&date_time" . $dateTime;
 
         return $this->sendRequest("GET", $url);
     }
@@ -97,12 +111,16 @@ class Mobiforte
 
     /** 
      * Schedule sms to be sent in future.
+     * @param $to is the destination number to deliver the message.
+     * @param $message is the kind of message ot deliver.
+     * @param $dateTime is the timestamp to send the message.
      */
-    public function schedule($date_time, $to = null, $message = null)
+    public function schedule($to = null, $message = null, $dateTime)
     {
-        return $this->send($to, $message, $date_time);
+        return $this->send($to, $message, $dateTime);
     }
 
+    /** THis is where the request actually get sent to mobiforte api */
     protected function sendRequest(string $type, $url)
     {
         $ch = curl_init();
